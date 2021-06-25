e53a6bd5198ae69567b04a82f5ad03e2a1c1954d
husseinfathel <Hussein@1998>
2021-06-26 00:20:42 +0300

    using SQL expression

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import uvicorn as uvicorn
 from fastapi import FastAPI
 from models import Grade, Semesters, Modules, Mcqs, Base, engine, session, conn
 
@@ -16,6 +15,18 @@ def grade():
 
 @app.post('/grade/add/<stage>')
 def grade_add(stage):
-    return conn.execute('INSERT INTO Grade (stage) VALUES ({})'.format(stage))
+    conn.execute('INSERT INTO Grade (stage) VALUES ({})'.format(stage))
+    return {'insert': 'Done'}
 
 
+@app.get('/semesters')
+def semesters():
+    query = session.query(Semesters).all()
+    print(query)
+    return query
+
+
+@app.post('/semesters/add/<seme>')
+def grade_add(seme):
+    conn.execute('INSERT INTO Semesters (num) VALUES ({})'.format(seme))
+    return {'insert': 'Done'}
